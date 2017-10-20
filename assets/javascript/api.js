e91f650987f281b935286a2878abe3a3236a2ddd
Neil Dhand <neil_dhand@baylor.edu>
2017-10-20 13:14:07 -0500

    working js

--- assets/javascript/api.js
+++ assets/javascript/api.js
@@ -1,11 +1,17 @@
-var queryURL = "https://api.aerisapi.com/fires/closest?p=sacramento,ca&filter=critical&client_id=38Kg3ZDxs3eiKihJ1pDYW&client_secret=Y2dpsgFp0570JXPe7Aeeam57UYVsIt0dIpa0nIEd";
+$("#findFire").on("click", function(event){
+	event.preventDefault();
 
-//ajax call
-$.ajax({
+	var place = $("#userInput").val().trim();
+
+	var queryURL = "https://api.aerisapi.com/fires/closest?p=" + place + "&filter=critical&client_id=38Kg3ZDxs3eiKihJ1pDYW&client_secret=Y2dpsgFp0570JXPe7Aeeam57UYVsIt0dIpa0nIEd";
+
+	//ajax call
+	$.ajax({
 	url: queryURL,
 	method: "GET"
-}).done(function(result) {
+	}).done(function(result) {
 	console.log(result.response);
 	// create var for all things we want represented from json dump
-	var
-});
+	});
+})
+

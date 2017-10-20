af3990692139075ca54e53176361c8158c686fa4
Deva <deva.palasingam@gmail.com>
2017-10-20 17:03:36 -0500

    reformatted html

---
+++ assets/javascript/mainApp.js
@@ -0,0 +1,12 @@
+//Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBWu3-eZXIiXEsalI1MrYLha2IwBvWpw4I",
+    authDomain: "fireapp-1508430756843.firebaseapp.com",
+    databaseURL: "https://fireapp-1508430756843.firebaseio.com",
+    projectId: "fireapp-1508430756843",
+    storageBucket: "fireapp-1508430756843.appspot.com",
+    messagingSenderId: "78846809015"
+  };
+  
+  firebase.initializeApp(config);
+  //Initialize Firebase=====================================

91d4a2126d703e0cff8be853a933682528b405ce
Deva <deva.palasingam@gmail.com>
2017-10-20 15:25:57 -0500

    messing with index

---
+++ assets/javascript/googleMapApi.js
@@ -0,0 +1,17 @@
+function mapfun() {
+        var uluru = {lat: -25.363, lng: 131.044};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 4,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+
+        var secondMarker = new google.maps.Marker({
+          position: {lat: 30.26, lng: -97.74},
+          map: map
+        });
+
+      }

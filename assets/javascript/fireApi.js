393a826c2cddd38ca3c60050c624ba4df678c4b9
Deva <deva.palasingam@gmail.com>
2017-10-21 13:58:43 -0500

    finished google maps with markers

--- assets/javascript/fireApi.js
+++ assets/javascript/fireApi.js
@@ -2,7 +2,7 @@ function fireApi(inputLocation){
 
 	
 
-	var queryURL = "https://api.aerisapi.com/fires/within?p=" + inputLocation + "&radius=50mi&limit=10&filter=critical&client_id=38Kg3ZDxs3eiKihJ1pDYW&client_secret=Y2dpsgFp0570JXPe7Aeeam57UYVsIt0dIpa0nIEd";
+	var queryURL = "https://api.aerisapi.com/fires/within?p=" + inputLocation + "&radius=100mi&limit=10&filter=critical&client_id=38Kg3ZDxs3eiKihJ1pDYW&client_secret=Y2dpsgFp0570JXPe7Aeeam57UYVsIt0dIpa0nIEd";
 
 
 	function fireInfoObject (lat, lng) {
@@ -25,23 +25,30 @@ function fireApi(inputLocation){
 			userInputLocation.lng = result.response[i].relativeTo.long
 			console.log("user input location: " + userInputLocation);
 
-			console.log(result.response[i].place);
-			console.log(result.response[i].report.name); 
-			console.log(result.response[i].place.name + " " + result.response[i].place.state + " " + result.response[i].place.country + "<br>");
-			console.log("area: " + result.response[i].report.areaMI + " square miles" + "<br>");
-			console.log("Date and Time: " + result.response[i].report.dateTimeISO + "<br>");
-			console.log("cause: " + result.response[i].report.cause + "<br>");
-			console.log(result.response[i].relativeTo.distanceMI + " mile(s) away from zip" + "<br>");
+			var fireInfo = new fireInfoObject(result.response[i].loc.lat, result.response[i].loc.long);
+
+			nearbyFiresArray.push(fireInfo);
+
+			// console.log("fire info");
+			// console.log(fireInfo);
+
+			// console.log("fire latitude: " + result.response[i].loc.lat + "<br>");
+			// console.log("fire longitude: " + result.response[i].loc.long + "<br><br>");
+			// console.log(result.response[i].place);
+			// console.log(result.response[i].report.name); 
+			// console.log(result.response[i].place.name + " " + result.response[i].place.state + " " + result.response[i].place.country + "<br>");
+			// console.log("area: " + result.response[i].report.areaMI + " square miles" + "<br>");
+			// console.log("Date and Time: " + result.response[i].report.dateTimeISO + "<br>");
+			// console.log("cause: " + result.response[i].report.cause + "<br>");
+			// console.log(result.response[i].relativeTo.distanceMI + " mile(s) away from zip" + "<br>");
 			// console.log("relative latitude: " + result.response[i].relativeTo.lat + "<br>");
 			// console.log("relative longitude: " + result.response[i].relativeTo.long + "<br>");
-			console.log("fire latitude: " + result.response[i].loc.lat + "<br>");
-			console.log("fire longitude: " + result.response[i].loc.long + "<br><br>");
+			
 
 		}
 
-		console.log("about to call google: ");
-		console.log(userInputLocation);
-		googleMapApi(userInputLocation);
+		googleMapApi(userInputLocation, nearbyFiresArray);
+		console.log(nearbyFiresArray);
 
 	// create var for all things we want represented from json dump
 	});
